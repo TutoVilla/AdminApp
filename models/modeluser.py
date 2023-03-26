d1baf1011681bea52f09a3ef0d4e7e39b7c358b7
Jose Villa <josevilla410@gmail.com>
2023-03-26 00:41:00 +0000

    Logging session done with DB

---
+++ models/modeluser.py
@@ -0,0 +1,32 @@
+from models.entities.user import User
+class ModelUser():
+    
+    @classmethod
+    def login(self,db,user):
+        try:
+            cursor=db.cursor()
+            sql = """SELECT id, username, password, fullname FROM login 
+                    WHERE username = '{}'""".format(user.username)
+            cursor.execute(sql)
+            row=cursor.fetchone()
+            if row != None:
+                user = User(row[0],row[1],User.check_password(row[2],user.password),row[3])
+                return user
+            else:
+                return None
+        except Exception as ex:
+            raise Exception(ex)
+        
+    @classmethod
+    def get_by_id(self, db, id):
+        try:
+            cursor = db.cursor()
+            sql = "SELECT id, username, fullname FROM login WHERE id = {}".format(id)
+            cursor.execute(sql)
+            row = cursor.fetchone()
+            if row != None:
+                return User(row[0], row[1], None, row[2])
+            else:
+                return None
+        except Exception as ex:
+            raise Exception(ex)  
