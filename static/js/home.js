4bde8753c21c7f5ff1662fb64e7cf46069881645
Jose Villa <josevilla410@gmail.com>
2023-05-02 22:19:20 +0100

    front end modifications for better views and functionality

--- static/js/home.js
+++ static/js/home.js
@@ -1,27 +1,6 @@
-$(document).on('click', '.delete-account', function() {
-    var accountId = $(this).data('account-id');
-    
-    // Mostrar un diálogo modal de confirmación
-    var confirmed = confirm("¿Está seguro que desea eliminar esta cuenta?");
+const deleteAccount = document.getElementById("deleteAccount");
+
+deleteAccount.addEventListener('click',() =>{
+  var deleteID = deleteAccount.getAttribute("data-account-id");
   
-    if (confirmed) {
-      // Pedir la contraseña al usuario
-      var password = prompt("Ingrese su contraseña:");
-  
-      // Enviar la solicitud de eliminación de la cuenta al servidor
-      $.ajax({
-        url: '/eliminar-cuenta/' + accountId,
-        method: 'POST',
-        data: { password: password },
-        success: function(response) {
-          alert("La cuenta ha sido eliminada.");
-          // Actualizar la página
-          location.reload();
-        },
-        error: function(error) {
-          alert("No se pudo eliminar la cuenta. Verifique su contraseña y vuelva a intentarlo.");
-        }
-      });
-    }
-  });
-  
+})
