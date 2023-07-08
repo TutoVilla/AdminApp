18eae0065e11030edcf5f261d2eed4dd85f64f1f
Jose Villa <josevilla410@gmail.com>
2023-07-08 13:37:57 +0100

    Mods on the way that the imageis recreated

---
+++ Auto/recreate_app.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker-compose stop app
+docker-compose rm -f app
+docker rmi adminapp_app
+
+read -p "Do you want to perform system prune? (Y/n): " response
+response=${response:-Y}  # Si no se proporciona ninguna respuesta, se establece el valor predeterminado en "Y"
+
+if [[ $response =~ ^[Yy]$ ]]; then
+    docker system prune --all --volumes --force
+fi
+
+docker-compose build app
+docker-compose up -d app
