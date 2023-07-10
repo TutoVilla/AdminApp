1e36ac7f8302e1db0ef5a816b87e031e7b5960a5
Jose Villa <josevilla410@gmail.com>
2023-07-11 00:22:22 +0100

    responsive changes

--- Auto/recreate_app.sh
+++ Auto/recreate_app.sh
@@ -3,7 +3,7 @@
 docker-compose stop app
 docker-compose rm -f app
 docker rmi adminapp_app
-
+docker rmi adminapp-app
 read -t 5 -p "Do you want to perform system prune? (Y/n): " response
 response=${response:-Y}  # Si no se proporciona ninguna respuesta, se establece el valor predeterminado en "Y"
 
